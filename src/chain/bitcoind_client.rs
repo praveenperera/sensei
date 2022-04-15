5abc78af92d0d23209cca68e5d58b3f9781dd042
John Cantrell <johncantrell97@gmail.com>
2022-04-15 13:45:14 -0400

    chain manager works with trait objects instead of concrete BitcoindClient type

--- src/chain/bitcoind_client.rs
+++ src/chain/bitcoind_client.rs
@@ -75,7 +75,7 @@ pub enum Target {
     HighPriority,
 }
 
-impl BlockSource for &BitcoindClient {
+impl BlockSource for BitcoindClient {
     fn get_header<'a>(
         &'a self,
         header_hash: &'a BlockHash,
